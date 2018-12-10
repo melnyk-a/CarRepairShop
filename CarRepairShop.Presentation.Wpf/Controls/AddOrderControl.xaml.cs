8e320e6596888d79707abd88698f1ba9cbbe6a45
melnyk-a <melnyk.an.il@gmail.com>
2018-12-11 01:41:42 +0200

    Add presentation for AddOrder

---
+++ CarRepairShop.Presentation.Wpf/Controls/AddOrderControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CarRepairShop.Presentation.Wpf.Controls
+{
+    internal partial class AddOrderControl : UserControl
+    {
+        public AddOrderControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- CarRepairShop.Presentation.Wpf/ViewModels/MainWindowViewModel.cs
+++ CarRepairShop.Presentation.Wpf/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,28 @@
-﻿namespace CarRepairShop.Presentation.Wpf.ViewModels
-{
-    internal sealed class MainWindowViewModel
-    {
+﻿using CarRepairShop.Wpf.Commands;
+using CarRepairShop.Wpf.ViewModels;
+using System.Windows.Input;
 
+namespace CarRepairShop.Presentation.Wpf.ViewModels
+{
+    internal sealed class MainWindowViewModel : ViewModel
+    {
+        private readonly AddOrderViewModel addOrder;
+        private readonly ICommand addOrderCommand;
+        
+        private object current;
+
+        public MainWindowViewModel()
+        {
+            addOrder = new AddOrderViewModel();
+            addOrderCommand = new DelegateCommand(() => Current = addOrder);
+        }
+
+        public object Current
+        {
+            get => current;
+            private set => SetProperty(ref current, value);
+        }
+
+        public ICommand AddOrderCommand => addOrderCommand;
     }
 }
