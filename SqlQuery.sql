e899e383d3a4c0eaf2c5f4b2dee61613953874d2
melnyk-a <melnyk.an.il@gmail.com>
2018-12-10 22:59:42 +0200

    Task 2

---
+++ SqlQuery.sql
@@ -0,0 +1,10 @@
+﻿use CarRepairShop;
+go
+
+select Persons.Name + N' ' + Persons.Surname as Name, Cars.Model, Orders.StartDate
+from  Orders 
+	inner join Clients on (Orders.ClientId = Clients.Id)
+	inner join Persons on (Clients.PersonId = Persons.Id)
+	inner join Cars on (Orders.CarId = Cars.Id)
+where Orders.MechanicId is null;
+go
