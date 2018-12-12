bfff6f1f4b1aa93174dba7f07b3c79448cd60264
melnyk-a <melnyk.an.il@gmail.com>
2018-12-12 19:07:04 +0200

    Add presentation of person as separate view

--- CarRepairShop.Presentation.Wpf/ViewModels/DetailOrderViewModel.cs
+++ CarRepairShop.Presentation.Wpf/ViewModels/DetailOrderViewModel.cs
@@ -5,14 +5,18 @@ namespace CarRepairShop.Presentation.Wpf.ViewModels
 {
     internal sealed class DetailOrderViewModel
     {
+        private readonly PersonViewModel client;
+        private readonly PersonViewModel mechanic;
         private readonly Order order;
 
         public DetailOrderViewModel(Order order)
         {
             this.order = order;
+            client = new PersonViewModel(order.Client.Person);
+            mechanic = order.Mechanic != null ? new PersonViewModel(order.Mechanic) : null;
         }
 
-        public string Client => $"{order.Client.Person.Name} {order.Client.Person.Surname}";
+        public PersonViewModel Client => client;
 
         public string CarModel => order.Car.Model;
 
@@ -20,7 +24,7 @@ namespace CarRepairShop.Presentation.Wpf.ViewModels
 
         public string FinishDate => order.FinishDate.ToShortDateString();
 
-        public string Mechanic => order.Mechanic != null ? $"{order.Mechanic.Name} {order.Mechanic.Surname}" : string.Empty;
+        public PersonViewModel Mechanic => mechanic;
 
         public string Price => order.Price!=0?string.Format("{0:0.00}", order.Price) : string.Empty;
 
---
+++ CarRepairShop.Presentation.Wpf/ViewModels/PersonViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CarRepairShop.Domain.Models;
+
+namespace CarRepairShop.Presentation.Wpf.ViewModels
+{
+    internal sealed class PersonViewModel
+    {
+        private readonly Person person;
+
+        public PersonViewModel(Person person)
+        {
+            this.person = person;
+        }
+
+        public string Name => $"{person.Name} {person.Surname}";
+    }
+}
