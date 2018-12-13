d06c0ccc3e8749d749a3d7e52db7cb46cce2ff13
melnyk-a <melnyk.an.il@gmail.com>
2018-12-13 19:59:31 +0200

    Refactor OrderDto

--- CarRepairShop.Data.SqlClient/Dtos/OrderDto.cs
+++ CarRepairShop.Data.SqlClient/Dtos/OrderDto.cs
@@ -6,12 +6,12 @@ namespace CarRepairShop.Data.SqlClient.Dtos
     internal sealed class OrderDto
     {
         public int Id { get; set; }
+        public Car Car { get; set; }
         public Client Client { get; set; }
+        public string Description { get; set; }
+        public DateTime FinishDate { get; set; }
         public Person Mechanic { get; set; }
         public double Price { get; set; }
-        public Car Car { get; set; }
         public DateTime StartDate { get; set; }
-        public DateTime FinishDate { get; set; }
-        public string Description { get; set; }
     }
 }
