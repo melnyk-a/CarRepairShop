159afbf1d9fb2ae91f8b9b7b2700f7f76caec67e
melnyk-a <melnyk.an.il@gmail.com>
2018-12-13 18:15:25 +0200

    Refactor DetailOrderViewModel

--- CarRepairShop.Presentation.Wpf/ViewModels/DetailOrderViewModel.cs
+++ CarRepairShop.Presentation.Wpf/ViewModels/DetailOrderViewModel.cs
@@ -6,27 +6,30 @@ namespace CarRepairShop.Presentation.Wpf.ViewModels
     internal sealed class DetailOrderViewModel
     {
         private readonly PersonViewModel client;
+        private readonly string finishDate;
         private readonly PersonViewModel mechanic;
         private readonly Order order;
+        private readonly string price;
 
         public DetailOrderViewModel(Order order)
         {
             this.order = order;
             client = new PersonViewModel(order.Client.Person);
             mechanic = order.Mechanic != null ? new PersonViewModel(order.Mechanic) : null;
+            finishDate = order.FinishDate != default(DateTime) ? order.FinishDate.ToShortDateString() : string.Empty;
+            price = order.Price != 0 ? string.Format("{0:0.00}", order.Price) : string.Empty;
         }
-
-        public PersonViewModel Client => client;
 
         public string CarModel => order.Car.Model;
 
-        public string StartDate => order.StartDate.ToShortDateString();
+        public string FinishDate => finishDate;
 
-        public string FinishDate => order.FinishDate!=default(DateTime)?order.FinishDate.ToShortDateString(): string.Empty;
+        public PersonViewModel Client => client;
 
         public PersonViewModel Mechanic => mechanic;
 
-        public string Price => order.Price!=0?string.Format("{0:0.00}", order.Price) : string.Empty;
+        public string Price => price;
 
+        public string StartDate => order.StartDate.ToShortDateString();
     }
 }
