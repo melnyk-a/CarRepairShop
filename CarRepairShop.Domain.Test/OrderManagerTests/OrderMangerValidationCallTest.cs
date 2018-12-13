aaa42feab634ffab2e8b33dac974165caa7c9ce9
melnyk-a <melnyk.an.il@gmail.com>
2018-12-13 17:35:16 +0200

    Add test for check correct validation method calls

---
+++ CarRepairShop.Domain.Test/OrderManagerTests/OrderMangerValidationCallTest.cs
@@ -0,0 +1,97 @@
+﻿using CarRepairShop.Domain.Validators;
+using CarRepairShop.Domain.Validators.ValidationCommands.ValidationCommandFactories;
+using NUnit.Framework;
+
+namespace CarRepairShop.Domain.Test.OrderManagerTests
+{
+    [TestFixture]
+    internal sealed class OrderMangerValidationCallTest
+    {
+        [Test]
+        public void ValidateProperty_DescriptionCall()
+        {
+            var vallidator = new ValidatorMock();
+            IOrderManager orderManager = new OrderManager(null, new ValidationCommandFactory(vallidator));
+
+            orderManager.ValidateProperty("Description", string.Empty);
+
+            Assert.That(vallidator.Calls, Has.Exactly(1).EqualTo(nameof(Validator.ValidateDescription)));
+        }
+
+        [Test]
+        public void ValidateProperty_NameCall()
+        {
+            var vallidator = new ValidatorMock();
+            IOrderManager orderManager = new OrderManager(null, new ValidationCommandFactory(vallidator));
+
+            orderManager.ValidateProperty("Name", string.Empty);
+
+            Assert.That(vallidator.Calls, Has.Exactly(1).EqualTo(nameof(Validator.ValidateName)));
+        }
+
+        [Test]
+        public void ValidateProperty_NumberCall()
+        {
+            var vallidator = new ValidatorMock();
+            IOrderManager orderManager = new OrderManager(null, new ValidationCommandFactory(vallidator));
+
+            orderManager.ValidateProperty("Number", string.Empty);
+
+            Assert.That(vallidator.Calls, Has.Exactly(1).EqualTo(nameof(Validator.ValidateNumber)));
+        }
+
+        [Test]
+        public void ValidateProperty_ModelCall()
+        {
+            var vallidator = new ValidatorMock();
+            IOrderManager orderManager = new OrderManager(null, new ValidationCommandFactory(vallidator));
+
+            orderManager.ValidateProperty("Model", string.Empty);
+
+            Assert.That(vallidator.Calls, Has.Exactly(1).EqualTo(nameof(Validator.ValidateModel)));
+        }
+
+        [Test]
+        public void ValidateProperty_PhoneCall()
+        {
+            var vallidator = new ValidatorMock();
+            IOrderManager orderManager = new OrderManager(null, new ValidationCommandFactory(vallidator));
+
+            orderManager.ValidateProperty("Phone", string.Empty);
+
+            Assert.That(vallidator.Calls, Has.Exactly(1).EqualTo(nameof(Validator.ValidatePhone)));
+        }
+        [Test]
+        public void ValidateProperty_PriceCall()
+        {
+            var vallidator = new ValidatorMock();
+            IOrderManager orderManager = new OrderManager(null, new ValidationCommandFactory(vallidator));
+
+            orderManager.ValidateProperty("Price", string.Empty);
+
+            Assert.That(vallidator.Calls, Has.Exactly(1).EqualTo(nameof(Validator.ValidatePrice)));
+        }
+
+        [Test]
+        public void ValidateProperty_SurnameCall()
+        {
+            var vallidator = new ValidatorMock();
+            IOrderManager orderManager = new OrderManager(null, new ValidationCommandFactory(vallidator));
+
+            orderManager.ValidateProperty("Surname", string.Empty);
+
+            Assert.That(vallidator.Calls, Has.Exactly(1).EqualTo(nameof(Validator.ValidateSurname)));
+        }
+
+        [Test]
+        public void ValidateProperty_YearCall()
+        {
+            var vallidator = new ValidatorMock();
+            IOrderManager orderManager = new OrderManager(null, new ValidationCommandFactory(vallidator));
+
+            orderManager.ValidateProperty("Year", string.Empty);
+
+            Assert.That(vallidator.Calls, Has.Exactly(1).EqualTo(nameof(Validator.ValidateYear)));
+        }
+    }
+}
---
+++ CarRepairShop.Domain.Test/OrderManagerTests/ValidatorMock.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using CarRepairShop.Domain.Validators;
+
+namespace CarRepairShop.Domain.Test.OrderManagerTests
+{
+    internal sealed class ValidatorMock : IValidator
+    {
+        private readonly ICollection<string> calls = new List<string>();
+
+        public ICollection<string> Calls => calls;
+
+        private IEnumerable<string> AddCall([CallerMemberName] string methodName = null)
+        {
+            calls.Add(methodName);
+            return new string[0];
+        }
+
+        public IEnumerable<string> ValidateDescription(string description)
+        {
+            return AddCall();
+        }
+
+        public IEnumerable<string> ValidateModel(string model)
+        {
+            return AddCall();
+        }
+
+        public IEnumerable<string> ValidateName(string name)
+        {
+            return AddCall();
+        }
+
+        public IEnumerable<string> ValidateNumber(string number)
+        {
+            return AddCall();
+        }
+
+        public IEnumerable<string> ValidatePhone(string phone)
+        {
+            return AddCall();
+        }
+
+        public IEnumerable<string> ValidatePrice(string price)
+        {
+            return AddCall();
+        }
+
+        public IEnumerable<string> ValidateSurname(string surname)
+        {
+            return AddCall();
+        }
+
+        public IEnumerable<string> ValidateYear(string year)
+        {
+            return AddCall();
+        }
+    }
+}
